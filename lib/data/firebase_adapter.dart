bac5ca35e067fba70dfe4ea27a46f89418bb7377
Joseph Elliott <joey.w.elliott@gmail.com>
2017-11-05 21:14:11 -0600

    Add some files to help draw a path

---
+++ lib/data/firebase_adapter.dart
@@ -0,0 +1,3 @@
+// I'm not sure what this will contain in the future, but
+// this folder should container adapters for writing
+// information to the Firebase database.
---
+++ lib/home/home.dart
@@ -0,0 +1,7 @@
+// The home screen
+
+// todo Add a widget for the home screen
+
+// todo Include Google sign in
+
+// todo Config Firebase
