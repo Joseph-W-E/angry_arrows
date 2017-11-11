555ffce67e4483cd4e2b1d418d2cd1a3a0c2ce21
Joseph Elliott <joey.w.elliott@gmail.com>
2017-11-11 00:08:23 -0600

    Working on scrolling

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:angry_arrows/game/gesture.dart';
@@ -10,13 +11,10 @@ class Level extends Game {
   final Size dimensions;
   final LevelConfiguration config;
 
-  // Sprites
   Arrow _arrow;
   List<Crate> _crates;
-  //List<Platform> _platforms;
 
-  // User input
-  List<Point> _points;
+  List<Point> _points = [];
   GestureInterpreter _interpreter = new GestureInterpreter();
 
   Level({this.dimensions, this.config}) : assert(dimensions != null), assert(config != null) {
@@ -26,51 +24,20 @@ class Level extends Game {
   void _setupSprites() {
     _arrow = new Arrow(config.arrow);
     _crates = config.crates.map((config) => new Crate(config)).toList();
-    //_platforms = config.platforms.map((config) => new Platform(config)).toList();
   }
 
   @override
   void render(Canvas canvas) {
-    // render the hud
-    // todo
-
     // render the arrow
     _internalRender(canvas, _arrow);
 
     // render the crates
     _crates.forEach((crate) => _internalRender(canvas, crate));
-
-    // render the platforms
-    // todo
-  }
-
-  void _internalRender(Canvas canvas, SpriteComponent sprite) {
-    canvas.save();
-    sprite.render(canvas);
-    canvas.restore();
-    canvas.save();
   }
 
   @override
   void update(double t) {
-    // we have all of the user input, so we can interpret their gesture
-
-    // if the input resides on the arrow, we should be in an "arrow firing" phase
-
-    // if the input is a noticeable drag and we weren't in the "arrow firing" phase,
-    // then we should be moving the viewport
-
-    // if the input is a noticeable drag and we were in the "arrow firing" phase,
-    // then we should be prepping the arrow for launch (adjusting angle, drawback)
-
-    // if the input was in the "arrow firing" phase and came to a hard stop, we should
-    // fire the arrow and move to the "arrow fired" phase
-
-    // if we are in the "arrow fired" phase, then we should ignore user input
-    // and we should be moving objects around based off the viewport
-
-    // defer to clearing points so each [update] has a fresh read of user input
-    _interpreter.interpret(_points);
+    _handleGesture(t, _interpreter.interpret(_points));
     _points.clear();
   }
 
@@ -86,11 +53,28 @@ class Level extends Game {
     // todo figure out how to destroy the game and return to normal flutter
   }
 
-  void _handleGesture(Gesture gesture) {
-    // sometimes we may want to ignore potentially bad gestures
-    if (gesture.isNaive) return;
+  void _handleGesture(double t, Gesture gesture) {
+    // we should ignore potentially bad gestures
+    // unless they're trying to grab the arrow
+    if (gesture == null || (gesture.isNaive && gesture is! ControlArrow)) return;
 
-    // do something with the gesture
+    if (gesture is Scroll) {
+      // move all the objects using the [Scroll]'s info
+      _arrow.x += gesture.distance;
+    } else if (gesture is ControlArrow) {
+      // move the arrow using the [ControlArrow]'s info
+    } else if (gesture is LaunchArrow) {
+      // move the objects and disable user input
+    } else {
+      print("Error: Unknown gesture $gesture");
+    }
+  }
+
+  void _internalRender(Canvas canvas, SpriteComponent sprite) {
+    canvas.save();
+    sprite.render(canvas);
+    canvas.restore();
+    canvas.save();
   }
 }
 
--- lib/game/gesture.dart
+++ lib/game/gesture.dart
@@ -1,34 +1,75 @@
 /// Interprets arrays of [Points] to determine what the user was trying to do.
 /// Keeps a local history of the gestures, allowing smart gesture detection.
 class GestureInterpreter {
-  List<Gesture> _history;
+  /// Used to determine more complicated [Gesture]s.
+  /// If the history is used to determine a [Gesture],
+  /// then [_history] should be cleared.
+  List<Gesture> _history = [];
 
-  GestureInterpreter();
+  // information used to determine if we tapped inside the arrow
+  Point arrowPoint;
+  double arrowWidth;
+  double arrowHeight;
+  double arrowRadians;
+
+  /// Used to help determine how to interpret some points in [interpret]
+  State _state = State.none;
+
+  GestureInterpreter({this.arrowPoint, this.arrowWidth, this.arrowHeight, this.arrowRadians});
 
   /// Converts a list of points to a gesture.
   /// If [_history] is empty or no smart gesture could be built,
   /// a naive gesture is returned.
   Gesture interpret(List<Point> points) {
-    return null; // todo
+    if (points == null || points.isEmpty) return null;
+
+    if (_history.isEmpty && _pointCollidesWithArrow(points.first)) {
+      return new ControlArrow(true);
+    }
+
+    points.forEach((point) => _history.add(new Gesture(true, point: point)));
+
+    return _analyzeHistory();
+  }
+
+  Gesture _analyzeHistory() {
+    Point start = _history.first.point; // todo
+    return new Scroll(false, distance: _history.map((gesture) => gesture.point).fold(0.0, (prev, point) => prev += start.x - point.x));
+  }
+
+  bool _pointCollidesWithArrow(Point point) {
+    return false; // todo
   }
 }
 
 /// Interpretation for a user's action.
 class Gesture {
-  /// A naive gesture could be ignored, depending on the situation.
-  bool get isNaive => false;
+  final bool isNaive;
+  final Point point;
+  Gesture(this.isNaive, {this.point});
 }
 
-/// The user is intending to scroll in the given [direction].
-class Scroll extends Gesture {}
+/// The user is intending to scroll at the [angle] the [distance].
+class Scroll extends Gesture {
+  final double distance;
+  Scroll(bool naive, {this.distance}) : super(naive);
+}
 
 /// The user is trying move the arrow (change [angle] and [position]).
-class ControlArrow extends Gesture {}
+class ControlArrow extends Gesture { // todo figure out what we need to make this work
+  ControlArrow(bool naive) : super(naive);
+}
 
 /// The user is intending to launch the arrow.
-class LaunchArrow extends Gesture {}
+class LaunchArrow extends Gesture {
+  LaunchArrow(bool naive) : super(naive);
+}
 
 class Point {
   final double x, y;
   Point({this.x, this.y}) : assert(x != null), assert(y != null);
+}
+
+enum State {
+  none, holdingArrow, launchingArrow
 }
