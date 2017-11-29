c9794f5f657d65836d4b5a83c7cd2c63ee223336
Joseph Elliott <joey.w.elliott@gmail.com>
2017-11-28 21:52:21 -0600

    add google play services support

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,35 @@ import 'dart:ui';
 import 'package:angry_arrows/game/game.dart';
 import 'package:angry_arrows/game/level.dart';
 import 'package:flame/flame.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 Future<Null> main() async {
+  final googleSignIn = new GoogleSignIn();
+
+  // attempt a google sign in
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+  if (user == null) {
+    await googleSignIn.signIn();
+  }
+
+  // setup Flame
   Flame.util.enableEvents();
   Flame.audio.disableLog();
 
+  // setup the levels
   var dimensions = await Flame.util.initialDimensions();
   var levels = new Levels(dimensions);
 
+  // start the game
   var game = new Level(
     dimensions: dimensions,
     config: levels.level2,
   )..start();
 
+  // start handling user input
   window.onPointerDataPacket = (PointerDataPacket packet) {
     var pointer = packet.data.first;
     game.input(pointer.physicalX, pointer.physicalY);
